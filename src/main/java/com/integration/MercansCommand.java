43b509e30421b760ddda926b4f0c2cae9f742000
hiwiton <hiwiton>
2023-08-13 18:28:53 +0000

    Initial commit

---
+++ src/main/java/com/integration/MercansCommand.java
@@ -0,0 +1,28 @@
+package com.integration;
+
+import io.micronaut.configuration.picocli.PicocliRunner;
+import io.micronaut.context.ApplicationContext;
+
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+import picocli.CommandLine.Parameters;
+
+@Command(name = "mercans", description = "...",
+        mixinStandardHelpOptions = true)
+public class MercansCommand implements Runnable {
+
+    @Option(names = {"-v", "--verbose"}, description = "...")
+    boolean verbose;
+
+    public static void main(String[] args) throws Exception {
+        PicocliRunner.run(MercansCommand.class, args);
+    }
+
+    public void run() {
+        // business logic here
+        if (verbose) {
+            System.out.println("Hi!");
+        }
+    }
+}
